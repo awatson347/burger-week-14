cda7c002356231f5d3fde291b8cb4530f986d60f
Alex Watson <awatson347@aol.com>
2016-10-03 21:37:41 -0400

    first

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (`burger_name`, `devoured`) VALUES ('Big Mac', 0);
+INSERT INTO burgers (`burger_name`, `devoured`) VALUES ('Whopper', 0);
+INSERT INTO burgers (`burger_name`, `devoured`) VALUES ('Teriyaki Chicken', 0);
+INSERT INTO burgers (`burger_name`, `devoured`) VALUES ('Double-Double, Animal-Style', 0);
+INSERT INTO burgers (`burger_name`, `devoured`) VALUES ('Beef and Cheddar', 0);
